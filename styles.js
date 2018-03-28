256966fee96ac00735f375b6f2ae6b4efb290313
Lee Graham <graham.la3@gmail.com>
2018-03-27 18:40:59 -0600

    Finalize Phase 3

--- styles.js
+++ styles.js
@@ -8,27 +8,39 @@ var cardTitle = document.querySelector('h2');
 var cardUrl = document.querySelector('a');
 var readButton = document.getElementById('read-button');
 var deleteButton = document.getElementById('delete-button');
+var counter = document.getElementById('counter');
+var reader = document.getElementById('reader')
+var formField = document.querySelector('form');
+var countCountula = 0; 
+// var countReadula = 0;
 
 // -----------------listeners------------------
-
+formField.addEventListener('keyup', enterBtnDisable)
 enterButton.addEventListener('click', addBookmark);
 mainSection.addEventListener('click', readToggle);
 mainSection.addEventListener('click', removeBookmark);
 
 
-
-
 // -----------functionality-------------
 
+function enterBtnDisable() {
+  if (websiteTitle.value > "" || websiteUrl.value > "") {
+  enterButton.disabled = false;
+  } else {
+    enterButton.disabled = true;
+  }
+}
+
 function addBookmark() {
-  console.log('lalalala');
+  countCountula++;
+  counter.innerText = `# of bookmarks: ${countCountula}`;
   if (websiteTitle.value > "" && websiteUrl.value > "") {
   var createdArticle = document.createElement('article');
   var mainSection = document.querySelector('main');
   var userInputInfo=
      `<h2>${websiteTitle.value}</h2>
       <hr />
-      <a href="${websiteUrl.value}" target="_blank">${websiteUrl.value}</a>
+      <a href="http://${websiteUrl.value}" target="_blank">${websiteUrl.value}</a>
       <hr />
       <button id="read-button" class="article-buttons" type="submit" name="Read">Read</button>
       <button id="delete-button" class="article-buttons delete-button" type="submit" name="Delete">Delete</button>`;
@@ -36,30 +48,32 @@ function addBookmark() {
     mainSection.appendChild(createdArticle);
     clearInputField();
   } else {
-    websiteTitle.disable = true;
-    websiteUrl.disable = true;
+    countCountula--;
+    counter.innerText = `# of bookmarks: ${countCountula}`;
     alert('ERROR, PLEASE FILL OUT BOTH FIELDS!')
   }
 }
 
 function readToggle() {
   var cardReadTarget = event.target.closest('article');
-  cardReadTarget.classList.toggle('read');
+  if (event.target.name === "Read") {
+    cardReadTarget.classList.toggle('read');
   var buttonTarget = event.target.closest('button')
-  buttonTarget.classList.toggle('color');
-  // var urlTarget = even.target.closest('a');
-  // urlTarget.classList.toggle('underline');
+    buttonTarget.classList.toggle('color');
+  }
+  readOrUnread = document.querySelectorAll('.read');
+  reader.innerText = `# of Bookmarks Read: ${readOrUnread.length}`;
 }
 
 function removeBookmark() {  
-  var cardDeleteTarget = event.target.closest('article')
-  console.log(event.target.name)
+    // readOrUnread = document.querySelectorAll('.read');
+  var cardDeleteTarget = event.target.closest('article');
   if (event.target.name === "Delete") {
+    countCountula--;
+    counter.innerText = `# of Bookmarks: ${countCountula}`;
+    // reader.innerText = `# of Bookmarks Read: ${readOrUnread.length}`;
     cardDeleteTarget.remove();
   }
-  
-  // removeTarget.remove(this.target);
-  // console.log(this.target);
 }
 
 function clearInputField() {
